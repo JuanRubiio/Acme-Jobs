6b37b56e5da5f35c71686561a7b1b0aa5388c097
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2019-10-20 12:52:01 +0200

    arreglando merge

--- src/main/java/acme/features/anonymous/bulletin/AnonymousBulletinListService.java
+++ src/main/java/acme/features/anonymous/bulletin/AnonymousBulletinListService.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * AnonymousShoutListService.java
- *
- * Copyright (c) 2019 Rafael Corchuelo.
- *
- * In keeping with the traditional purpose of furthering education and research, it is
- * the policy of the copyright owner to permit non-commercial use and redistribution of
- * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes. The copyright owner does not offer any warranties or representations, nor do
- * they accept any liabilities with respect to them.
- */
->>>>>>> task006
 
 package acme.features.anonymous.bulletin;
 
@@ -20,11 +6,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-import acme.entities.bulletin.Bulletin;
-=======
 import acme.entities.bulletins.Bulletin;
->>>>>>> task006
 import acme.framework.components.Model;
 import acme.framework.components.Request;
 import acme.framework.entities.Anonymous;
@@ -33,11 +15,8 @@ import acme.framework.services.AbstractListService;
 @Service
 public class AnonymousBulletinListService implements AbstractListService<Anonymous, Bulletin> {
 
-<<<<<<< HEAD
-=======
 	// Internal state ---------------------------------------------------------
 
->>>>>>> task006
 	@Autowired
 	AnonymousBulletinRepository repository;
 
@@ -50,25 +29,6 @@ public class AnonymousBulletinListService implements AbstractListService<Anonymo
 	}
 
 	@Override
-<<<<<<< HEAD
-	public void unbind(final Request<Bulletin> request, final Bulletin entity, final Model model) {
-		assert request != null;
-		assert entity != null;
-		assert model != null;
-
-		request.unbind(entity, model, "bulletinName", "type", "body");
-
-	}
-
-	@Override
-	public Collection<Bulletin> findMany(final Request<Bulletin> request) {
-		assert request != null;
-		Collection<Bulletin> result;
-
-		result = this.repository.findAllBulletin();
-
-		return result;
-=======
 	public Collection<Bulletin> findMany(final Request<Bulletin> request) {
 		assert request != null;
 
@@ -86,7 +46,6 @@ public class AnonymousBulletinListService implements AbstractListService<Anonymo
 
 		request.unbind(entity, model, "bulletinName", "type", "body");
 
->>>>>>> task006
 	}
 
 }
--- src/main/java/acme/features/anonymous/bulletin/AnonymousBulletinRepository.java
+++ src/main/java/acme/features/anonymous/bulletin/AnonymousBulletinRepository.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * AnonymousUserAccountRepository.java
- *
- * Copyright (c) 2019 Rafael Corchuelo.
- *
- * In keeping with the traditional purpose of furthering education and research, it is
- * the policy of the copyright owner to permit non-commercial use and redistribution of
- * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes. The copyright owner does not offer any warranties or representations, nor do
- * they accept any liabilities with respect to them.
- */
->>>>>>> task006
 
 package acme.features.anonymous.bulletin;
 
@@ -20,29 +6,13 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
-import acme.entities.bulletin.Bulletin;
-=======
 import acme.entities.bulletins.Bulletin;
->>>>>>> task006
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
 public interface AnonymousBulletinRepository extends AbstractRepository {
 
 	@Query("select b from Bulletin b")
-<<<<<<< HEAD
-	Collection<Bulletin> findAllBulletin();
-
-	@Query("select b from Bulletin b where b.name = ?1")
-	Collection<Bulletin> findByName(String name);
-
-	@Query("select b from Bulletin b where b.surname = ?1")
-	Collection<Bulletin> findBySurname(String surname);
-
-	@Query("select b from Bulletin b where b.nif = ?1")
-	Collection<Bulletin> findByNif(String nif);
-=======
 	Collection<Bulletin> findMany();
 
 	@Query("select b from Bulletin b where b.name= ?1")
@@ -54,5 +24,4 @@ public interface AnonymousBulletinRepository extends AbstractRepository {
 	@Query("select b from Bulletin b where b.dni= ?1")
 	Collection<Bulletin> findByDni(String dni);
 
->>>>>>> task006
 }
